9c313b7473d5acad5e2c13d0ede3a892728c6d4b
Alexandra Sandulescu <alecsandra.sandulescu@gmail.com>
2015-08-15 14:34:20 +0300

    Extended owtf - optimization for the update step
    Download optional tools archives at build time
    Install optional tools at runtime for --update

--- optional_tools.sh
+++ optional_tools.sh
@@ -1,15 +1,20 @@
-apt-get install -y theharvester \
-                   tlssled \
-                   nikto \
-                   dnsrecon \
-                   nmap \
-                   whatweb \
-                   skipfish \
-                   w3af-console \
-                   dirbuster \
-                   metasploit \
-                   wpscan \
-                   wapiti \
-                   waffit \
-                   whatweb \
-                   hydra
+PACKAGES="theharvester \
+          tlssled \
+          nikto \
+          dnsrecon \
+          nmap \
+          whatweb \
+          skipfish \
+          w3af-console \
+          dirbuster \
+          metasploit \
+          wpscan \
+          wapiti \
+          waffit \
+          hydra"
+
+if [ "$1" = "--download-only" ]; then
+  apt-get install -d -y $PACKAGES
+else
+  apt-get install -y $PACKAGES
+fi

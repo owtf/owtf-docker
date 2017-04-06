4ac0861f914634025f4c4c2dc9d1975188f3a0c7
Conrad Stein K <conrad@devaid.cl>
2017-04-06 00:57:10 -0300

    [release] Fixes the image for the new OWTF release changes.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM kalilinux/kali-linux-docker
 
-MAINTAINER @delta24 viyat001@gmail.com, @alexandrasandulescu alecsandra.sandulescu@gmail.com
+MAINTAINER @delta24 viyat.bhalodia@owasp.org, @alexandrasandulescu alecsandra.sandulescu@gmail.com
 
 RUN apt-get update --fix-missing && apt-get upgrade -y
 
@@ -15,12 +15,8 @@ RUN apt-get autoremove
 # dowload optional packages archives
 COPY optional_tools.sh /usr/bin/
 RUN chmod +x /usr/bin/optional_tools.sh
-RUN /bin/bash /usr/bin/optional_tools.sh --download-only
 
-# upgrade pip and install required python packages
-COPY owtf.pip /
-RUN ["pip", "install", "--upgrade", "pip"]
-RUN ["pip", "install", "--upgrade", "-r", "owtf.pip"]
+RUN /bin/bash /usr/bin/optional_tools.sh
 
 #Kali SSL lib-fix
 ENV PYCURL_SSL_LIBRARY openssl
@@ -28,22 +24,15 @@ ENV PYCURL_SSL_LIBRARY openssl
 #download latest OWTF
 RUN git clone -b develop https://github.com/owtf/owtf.git
 RUN mkdir owtf/tools/restricted
-# allow access to the web ui from outside
-COPY framework_config.cfg.patch owtf/
-COPY default.cfg.patch owtf/
+
+ENV TERM xterm
+ENV SHELL /bin/bash
 
 # core installation
-RUN python owtf/install/install.py --no-user-input --core-only
-
-# DB installation and setup
-COPY postgres_entry.sh owtf/scripts/
-RUN chmod +x owtf/scripts/postgres_entry.sh
+RUN python owtf/install/install.py --no-user-input
 
 # expose ports
 EXPOSE 8010 8009 8008
-
-# cleanup
-RUN rm packages.sh owtf.pip
 
 #setup postgres
 USER postgres
@@ -52,6 +41,9 @@ VOLUME  ["/etc/postgresql", "/var/log/postgresql", "/var/lib/postgresql"]
 # setting user to root
 ENV USER root
 USER root
+
+# Copy postgres_entry to scripts
+COPY postgres_entry.sh owtf/scripts
 
 #set entrypoint
 COPY owtf_entry.sh /usr/bin/
--- postgres_entry.sh
+++ postgres_entry.sh
@@ -40,7 +40,7 @@ FILE_PATH=$(readlink -f "$0")
 SCRIPTS_DIR=$(dirname "$FILE_PATH")
 RootDir=${1:-$(dirname "$SCRIPTS_DIR")}
 
-config_file="$RootDir/framework/config/framework_config.cfg"
+config_file="/root/.owtf/configuration/framework_config.cfg"
 db_config_file="$(get_config_value DATABASE_SETTINGS_FILE $config_file)"
 
 # Saved postgres settings
@@ -59,53 +59,31 @@ PGLOG=$PGDATA/serverlog
 mkdir -p /var/run/postgresql && chown -R postgres /var/run/postgresql
 mkdir -p $PGDATA && chown -R postgres $PGDATA
 
-# Restart postgres service.
-systemctl_bin=$(which systemctl | wc -l 2> /dev/null)
-if [ "$systemctl_bin" = "1" ]; then
-  systemctl restart postgresql
-else
-  START_CMD="/usr/lib/postgresql/${postgres_version}/bin/pg_ctl -w \
-    -D $PGDATA \
-    -o \"-c config_file=/etc/postgresql/${postgres_version}/main/postgresql.conf\" \
-    restart"
-  su postgres -c "$START_CMD" >> $PGLOG 2> $PGLOG
-fi
-
-postgresql_fix() {
-  # remove SSL=true from the postgresql main config
-  postgres_version="$(psql --version 2>&1 | tail -1 | awk '{print $3}' | sed 's/\./ /g' | awk '{print $1 "." $2}')"
-  postgres_conf="$(echo 'SHOW config_file;' | sudo -u postgres psql | grep 'postgres')"
-  echo "Having SSL=true in postgres config causes many errors (psycopg2 problem)"
-  # patched for docker installation
-  remove_ssl="y"  # tolower
-  case $remove_ssl in
-    [yY][eE][sS]|[yY])
-      sed -i -e '/ssl =/ s/= .*/= false/' $postgres_conf
-
-      echo "Restarting the postgresql service"
-      # get the return values of which commands to determine the service controller
-      which service  >> /dev/null 2>&1
-      service_bin=$?
-      which systemctl  >> /dev/null 2>&1
-      systemctl_bin=$?
-      if [ "$service_bin" != "1" ]; then
-        service postgresql restart
-        service postgresql status | grep -q '^Running clusters: ..*$'
-        status_exitcode="$?"
-      elif [ "$systemctl_bin" != "1" ]; then
-        systemctl restart postgresql
-        systemctl status postgresql | grep -q "active"
-        status_exitcode="$?"
-      else
-        echo "[+] It seems postgres server is not running or responding, please start/restart it manually!"
-        exit 1
-      fi
-      ;;
-    *)
-      # do nothing
-      ;;
-  esac
+postgresql_start() {
+  echo "Starting the postgresql service"
+  # get the return values of which commands to determine the service controller
+  which service  >> /dev/null 2>&1
+  service_bin=$?
+  which systemctl  >> /dev/null 2>&1
+  systemctl_bin=$?
+  if [ "$service_bin" != "1" ]; then
+    service postgresql start
+    service postgresql status | grep -q '^Running clusters: ..*$'
+    status_exitcode="$?"
+  elif [ "$systemctl_bin" != "1" ]; then
+    systemctl start postgresql
+    systemctl status postgresql | grep -q "active"
+    status_exitcode="$?"
+  elif [ "$systemctl_bin" != "0" ] && [ "$service_bin" != "0" ]; then
+      echo "[+] Using pg_ctlcluster to start the server."
+      sudo pg_ctlcluster ${postgres_version} main start
+  else
+      echo "[+] We couldn't determine how to start the postgres server, please start it and rerun this script"
+      exit 1
+  fi
 }
+
+postgresql_start
 
 # Clean db before creating it.
 check_owtf_db=$(su - postgres -c "psql -l | grep -w $saved_server_dbname | grep -w $saved_server_user | wc -l")
@@ -114,7 +92,5 @@ if [ "$check_owtf_db" != "0" ]; then
     clean_postgres $saved_server_dbname $saved_server_user
 fi
 
-# Fix postgresql config
-postgresql_fix
 
 init_postgres $saved_server_dbname $saved_server_user $saved_server_pass

0484be2e1efedfbcaa8a4486fa9ecfeee67b0e37
Viyat Bhalodia <viyatb@users.noreply.github.com>
2017-04-08 15:23:45 -0400

    [release] Fixes the image for the new OWTF release changes. (#29)

--- owtf_entry.sh
+++ owtf_entry.sh
@@ -8,7 +8,6 @@ function parse_arg {
     echo "Usage : $0 [OPTIONS]" >&2
     echo -e "-h, --help\n\tDisplay help and exit" >&2
     echo -e "-u, --update\n\tInstall optional dependencies" >&2
-    echo -e "-u, --exposed\n\tAllow access to the web ui. Recommended when a virtual host is used" >&2
     return
   fi
 
@@ -17,17 +16,11 @@ function parse_arg {
     /bin/bash /usr/bin/optional_tools.sh
     echo "[*] OWTF optional dependencies were successfully installed" >&2
   fi
-
-  if [[ "$1" == "--exposed" ]] || [[ "$1" == "-e" ]]; then
-    echo "[*] Make sure you run this image with the proper parameters" >&2
-    echo "[*] #docker run -it -p 8008:8008 -p 8009:8009 -p 8010:8010 --privileged <image_name>" >&2
-    patch owtf/framework/config/framework_config.cfg -i owtf/framework_config.cfg.patch
-    patch owtf/profiles/general/default.cfg -i owtf/default.cfg.patch
-  fi
-
-  rm -f owtf/default.cfg.patch
-  rm -f owtf/framework_config.cfg.patch
 }
+
+# Patch server address so that they are exposed to the host
+sed -i 's@INBOUND_PROXY_IP: 127.0.0.1@INBOUND_PROXY_IP: 0.0.0.0@' ${HOME}/.owtf/configuration/general.cfg
+sed -i 's@SERVER_ADDR: 127.0.0.1@SERVER_ADDR: 0.0.0.0@' ${HOME}/.owtf/configuration/framework_config.cfg
 
 # Start postgres server and configure db.
 sh owtf/scripts/postgres_entry.sh
@@ -43,5 +36,5 @@ if [ $# -gt 0 ]; then
 fi
 
 # Run owtf.
-cd owtf
-./owtf.py
+source ~/.bashrc; workon owtf
+./owtf/owtf.py
